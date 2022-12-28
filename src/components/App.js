297b31d4ed85105508d027184dd8d9912c7fe525
Saurabh <saurabhgawande2016@gmail.com>
2022-12-28 22:12:48 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,27 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+ const [text , useText] = useState("");
+ function fun(){
+  
+  useText (
+    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+  );
+ }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={fun}>click</button>
+      {text}
     </div>
   );
 }
 
 
 export default App;
+
+
+
+
+
+
+
